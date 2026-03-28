f9e680f66ab5a25c5683d8b3daca7265d38cca55
Nexora Technologies <nexoratechnologies.ke@gmail.com>
2026-03-28 12:42:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3961368112-69f97",
+  "appId": "1:489426071490:web:c8d775ab8700d92952f05b",
+  "apiKey": "AIzaSyAroHao0_kPNS6mB0GoEvCvZfisiHCx9bY",
+  "authDomain": "studio-3961368112-69f97.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489426071490"
+};
